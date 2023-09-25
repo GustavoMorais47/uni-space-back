03a9c0102c636f89125176739f9bb239eaf29799
Gustavo Morais <gustavomorais47.gm@gmail.com>
2023-09-25 14:44:38 -0300

    deleted: Exclusção das tipagens não utilizadas

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,23 +1,11 @@
 import { Document } from "mongoose";
 
-export interface CorType {
-  vermelho: number;
-  verde: number;
-  azul: number;
-  transparencia: number;
-}
-
 export enum Role {
   ADMIN = "admin",
   LABS = "labs",
   INFRA = "infra",
   PROFESSOR = "professor",
   ALUNO = "aluno",
-}
-
-export enum ServicosEnum {
-  SUPPORT = "Support",
-  MANAGE_SPACES = "Manage_Spaces",
 }
 
 export interface UserType extends Document {
@@ -32,14 +20,4 @@ export interface UserType extends Document {
 export interface PayloadType {
   id: string;
   role: Role;
-}
-
-export interface ServicosType {
-  id: ServicosEnum;
-  titulo: string;
-  imagem: string;
-  cor_fundo: CorType;
-  cor_fonte: CorType;
-  necessita_conexao: boolean;
-  status: boolean;
 }
